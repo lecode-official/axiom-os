731ed8ccf37ecd7db8e813c802e1823dec7b870b
David Neumann <david.neumann@lecode.de>
2015-09-15 23:24:39 +0200

    Now the the virtual hard disk to which the operating system is written has been formatted to FAT12 (because it is simpler, but may change in the future).

--- Boot/BootSector.asm
+++ Boot/BootSector.asm
@@ -6,13 +6,18 @@ bits  16
 ; 0x7C00, therefore all our addresses have to be aligned to this
 org   0x7C00
 
-; We have organized our addresses to 0x7C00, this means all addresses are based from 0x7c00:0, because the data segments are within
-; the same code segment, they are nulled
+; Pads the beginning of the bootloader with 62 bytes of zeros, this is needed, because the bootloader is written to a FAT12 filesystem,
+; the first 62 bytes of the boot sector contain the FAT12 headers
+times 62                db 0
+
+; We have organized our addresses to 0x7C00, this means all addresses are based from 0x7c00:0, because the data segments are within the
+; same code segment, they are nulled
 xor   ax, ax                      ; Sets the AX register to 0 by performing an exclusive or operation on it
 mov   ds, ax                      ; Sets the data segment to 0
 mov   es, ax                      ; Sets the extra segment to 0
 
-; Sets up the stack safely away from the code
+; Sets up the stack safely away from the code at the address 0x9000 (the stack grows from higher address downwards to lower addresses,
+; therefore a stack overflow would result in the code of the boot sector to be overwritten, which will result in strange behavior)
 mov   bp, 0x9000                  ; Sets the bottom of the stack
 mov   sp, bp                      ; Sets the top of the stack (since the stack is empty at the beginning, this is the same as the
                                   ; stack's bottom)
@@ -21,8 +26,8 @@ mov   sp, bp                      ; Sets the top of the stack (since the stack i
 mov   si, BootSectorLoadedMessage
 call  PrintString
 
-; In order to prevent the CPU from going on beyond the boot loader and potentially executing random bytes, the CPU is halted (but
-; it should not come this far)
+; In order to prevent the CPU from going on beyond the boot loader and potentially executing random bytes, the CPU is halted (but it
+; should not come this far)
 cli                               ; Clears all interrupts before halting the CPU
 hlt                               ; Prevents any further execution of code
 
@@ -34,8 +39,8 @@ hlt                               ; Prevents any further execution of code
 ; Contains all the strings that are used during the execution of the boot sector
 BootSectorLoadedMessage db "Success: The BIOS has loaded the first stage of the bootloader", 0
 
-; Pads the boot sector to 512 bytes (the boot sector must be exactly 512 bytes) with the last two bytes as the magic boot sector
-; number (the BIOS and the master boot record recognize bootable devices if the last to bytes of the boot sector are 0x55AA)
+; Pads the boot sector to 512 bytes (the boot sector must be exactly 512 bytes) with the last two bytes as the magic boot sector number
+; (the BIOS and the master boot record recognize bootable devices if the last to bytes of the boot sector are 0x55AA)
 times 510 - ($ - $$)    db 0
 dw    0xAA55                      ; x86 and AMD64 are little endian machines, which means that the most significant byte comes first
                                   ; (0x55AA => 0xAA55)
